6138cccc41700f80dc86e8a33d4603c8dd3b9e53
maxawolff <maxawolff@hotmail.com>
2018-01-23 11:18:55 -0800

    added all fields for card model

--- deckbuilder/card/models.py
+++ deckbuilder/card/models.py
@@ -5,5 +5,19 @@ from django.db import models
 class Card(models.Model):
     """Class for card model."""
 
+    COLORS = (['W', 'White'], ['U', 'Blue'], ['B', 'Black'],
+              ['R', 'Red'], ['G', 'Green'], ['C', 'Colorless'],
+              ['N', 'Generic'])
+    RARITIES = (['M', 'Mythic-Rare'], ['R', 'Rare'],
+                ['U', 'Uncommon'], ['C', 'Common'])
     cmc = models.CharField(max_length=20)
-
+    colors = models.CharField(max_length=20, choices=COLORS)
+    image = models.ImageField(upload_to='images')
+    loyalty = models.IntegerField(null=True, blank=True)
+    mana_Cost = models.CarField(max_length=20)
+    name = models.CharField(max_length=30)
+    power = models.IntegerField(null=True, blank=True)
+    toughness = models.IntegerField(null=True, blank=True)
+    rarity = models.CharField(max_length=20, choices=RARITIES)
+    card_text = models.CharField(max_length=300)
+    card_type = models.CharField(max_length=20)
