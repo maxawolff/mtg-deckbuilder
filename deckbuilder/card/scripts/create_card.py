61b96a23071d6d53ddf9cae509f29595d4bf7ee5
maxawolff <maxawolff@hotmail.com>
2018-01-25 13:12:23 -0800

    finished script to add all cards from rivals to the database, lets test it

--- deckbuilder/card/scripts/create_card.py
+++ deckbuilder/card/scripts/create_card.py
@@ -1,6 +1,6 @@
-from card import Card, Set
 from mtgsdk import Card as SourceCard
 from mtgsdk import Set as sourceSet
+from card import Card, Set
 
 def run(*args):
     """Add a card to the database from the mtgsdk."""
@@ -11,3 +11,21 @@ def run(*args):
         if not sets:
             return
         cur_set = sets[0]
+        rivals_cards = SourceCard.where(set='rix')
+        rivals_cards = rivals_cards.all()
+        for card in rivals_cards:
+            new_card = Card.objects.create(name=card.name,
+                                           colors=card.colors,
+                                           cmc=card.cmc,
+                                           image=card.image_url,
+                                           mana_cost=card.mana_cost,
+                                           rarity=card.rarity,
+                                           card_type=card.type,
+                                           )
+            if card.power:
+                new_card.power = card.power
+            if card.toughness:
+                new_card.toughness = card.toughness
+            if card.loyalty:
+                new_card.loyalty = card.loyalty
+            new_card.save()
